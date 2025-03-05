525b424632059de78099a4ae88fa9f7bf1217464
crisman9018 <cristianmantilla18@gmail.com>
2025-03-05 11:21:06 -0500

    Miercoles5_03_2025

--- Ejercicios python/clima.py
+++ Ejercicios python/clima.py
@@ -3,6 +3,7 @@ import json
 pagina = "https://api.openweathermap.org/data/2.5/weather?q=Piedecuesta,co&APPID=4491ad6d8e5c821739cbd71c337ec5c2&units=metric"
 page = requests.get(str(pagina))
 json_data = json.loads(page.text)
+print(json_data)
 print(json_data["main"]["temp"])
 print(json_data["weather"][0]["main"])
 print(json_data["weather"][0]["description"])
